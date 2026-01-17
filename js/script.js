630896ea6a8b0c91e45b07deeb45820f9ad2a10a
Len Tan Tran <L.TanTran@eml.com.au>
2026-01-18 06:55:55 +0700

    Initial commit

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY', 'NAME!' ],
+			strings: [ 'Happy', 'Birthday', 'Thu Hiền!' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
